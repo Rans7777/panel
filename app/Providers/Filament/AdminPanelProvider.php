c0bc88d7a4ce45d72075b80ae364f6e811cbdddc
Rans7777 <197063652+Rans7777@users.noreply.github.com>
2025-02-11 22:16:27 +0900

    feat: Add sortable plugin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,8 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use EightyNine\FilamentPageAlerts\FilamentPageAlertsPlugin;
+use IbrahimBougaoua\FilaSortable\FilaSortablePlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -53,6 +55,10 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                //FilamentPageAlertsPlugin::make(),
+                FilaSortablePlugin::make(),
             ]);
     }
 }
