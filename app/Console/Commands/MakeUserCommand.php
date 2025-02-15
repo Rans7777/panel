d7986eff1ddf0ec524f5acc3947c8974e567c0ee
Rans7777 <197063652+Rans7777@users.noreply.github.com>
2025-02-15 11:27:35 +0900

    make:userの改善

--- app/Console/Commands/MakeUserCommand.php
+++ app/Console/Commands/MakeUserCommand.php
@@ -7,7 +7,12 @@ namespace App\Console\Commands;
 use App\Models\User;
 use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\password;
+use function Laravel\Prompts\text;
 
 final class MakeUserCommand extends Command
 {
@@ -32,7 +37,7 @@ final class MakeUserCommand extends Command
      */
     public function handle()
     {
-        $username = $this->argument('username') ?: $this->ask('ユーザー名を入力してください');
+        $username = $this->argument('username') ?: text('<fg=green>ユーザー名を入力してください</>', default: 'admin', required: true);
 
         if (User::where('name', $username)->exists()) {
             $this->error("ユーザー名 \"{$username}\" は既に存在します。");
@@ -40,25 +45,25 @@ final class MakeUserCommand extends Command
             return 0;
         }
 
-        $password = $this->argument('password') ?: $this->secret('パスワードを入力してください');
-        $role = $this->argument('role') ?: $this->ask('付与するロールを入力してください');
+        $password = $this->argument('password') ?: password('<fg=green>パスワードを入力してください</>', required: true);
+        $role = $this->argument('role') ?: text('<fg=green>付与するロールを入力してください</>', required: true);
 
         $skipConfirmation = $this->argument('skip_confirmation') === 'yes';
-        if (!$skipConfirmation && !$this->confirm('この情報でユーザーを作成してよろしいですか？ [ユーザー名: '.$username.', ロール: '.$role.']')) {
+        if (!$skipConfirmation && !confirm('この情報でユーザーを作成してよろしいですか？ [ユーザー名: '.$username.', ロール: '.$role.']', default: false)) {
             $this->info('ユーザー作成をキャンセルしました。');
 
             return 0;
         }
 
         try {
-            $user = User::create([
-                'name' => $username,
-                'password' => Hash::make($password),
-            ]);
-
-            $user->assignRole($role);
-
-            $this->info('ユーザーが正常に作成されました！');
+            DB::transaction(function () use ($username, $password, $role) {
+                $user = User::create([
+                    'name' => $username,
+                    'password' => Hash::make($password),
+                ]);
+                $user->assignRole($role);
+                $this->info('ユーザーが正常に作成されました！');
+            });
         } catch (Exception $e) {
             $this->error('ユーザー作成中にエラーが発生しました: '.$e->getMessage());
         }
