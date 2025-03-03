7e65961631af22c69230fa3d413b472c68eede14
Rans7777 <197063652+Rans7777@users.noreply.github.com>
2025-03-03 12:37:13 +0900

    テストの追加

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * @var string
+     */
+    protected $model = Order::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'total_price' => $this->faker->numberBetween(100, 5000),
+            'product_id'  => Product::factory(),
+            'created_at'  => now(),
+            'updated_at'  => now(),
+        ];
+    }
+}
