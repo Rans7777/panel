cf4602e6d702177215e3f4f530bfef362a9702bb
Rans7777 <197063652+Rans7777@users.noreply.github.com>
2025-03-03 18:13:26 +0900

    テストを増やす (#25)
    * ログインテストの追加
    
    * 不要なインポート
    
    * テストを追加
    
    * テストの追加
    
    * テストの追加
    
    * テストの追加と修正
    
    * fix: pint
    
    * テストドキュメントの追加

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+final class OrderFactory extends Factory
+{
+    /**
+     * @var string
+     */
+    protected $model = Order::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'total_price' => $this->faker->numberBetween(100, 5000),
+            'product_id' => Product::factory(),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
