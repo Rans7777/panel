2c6083e441d23a09d5d7be5c8ecf5c80e4d3f2a6
Rans7777 <197063652+Rans7777@users.noreply.github.com>
2025-03-15 00:14:11 +0900

    変更

--- database/migrations/2025_03_14_140140_add_uuid_orders_column.php
+++ database/migrations/2025_03_14_140140_add_uuid_orders_column.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->char('uuid', 36)->after('id')->default(DB::raw('(UUID())'));
+            $table->char('uuid', 36)->after('id')->defaul(uuid_create());
         });
     }
 
