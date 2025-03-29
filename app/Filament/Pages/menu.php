d94d6ed8ce0ae8c29445b0e5d0f60c75f842887c
Rans7777 <197063652+Rans7777@users.noreply.github.com>
2025-03-29 16:27:34 +0900

    リストにメニューを追加 (#133)

---
+++ app/Filament/Pages/menu.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class menu extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.menu';
+
+    protected static ?string $navigationLabel = 'メニュー';
+
+    public function mount(): void
+    {
+        $this->redirect('/menu');
+    }
+}
